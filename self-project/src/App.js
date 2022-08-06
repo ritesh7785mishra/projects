7addbb7bf70da923ec10d1978a23d6f789aebb1b
Ritesh Mishra <ritesh7785mishra@gmail.com>
2022-08-06 13:44:26 +0530

    initial commit , amazon clone in making

---
+++ self-project/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from "./Header.js"
+function App() {
+  return (
+    // following BEM convention of classNames
+    <div className="app">
+      <Header></Header>
+      {/* home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ self-project/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAbJdvZgfg-nkdjFqP7Yrfm3bfLxeMvhAk",
+    authDomain: "challenge-feccb.firebaseapp.com",
+    projectId: "challenge-feccb",
+    storageBucket: "challenge-feccb.appspot.com",
+    messagingSenderId: "132903107899",
+    appId: "1:132903107899:web:01fc6e1b7a87fd7a620b1e",
+    measurementId: "G-SQD6XX79Z1"
+  };  
