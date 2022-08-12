a7f2d9ff5f9ddae20812069f73e0d3e0a0779dae
Ritesh Mishra <ritesh7785mishra@gmail.com>
2022-08-12 15:28:23 +0530

    adding functionality of adding and removing items from the basket

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,15 +1,38 @@
-
+import React from "react"
 import './App.css';
 import Header from "./Header.js"
 import Home from "./Home.js"
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
+import Checkout from './Checkout';
 
 function App() {
   return (
     // following BEM convention of classNames
-    <div className="app">
-      <Header></Header>
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="app">
+      <Header/>
+        <Routes>
+          <Route exact path="/login" element ={
+            <h1>Login page</h1>
+          }></Route>
+          <Route exact path="/"element={
+            <>
+            <Home/>
+            </>
+            }
+          />
+          <Route exact path="/checkout"element={
+            <>
+            <Checkout/>
+            </>
+            }
+          />
+         
+        </Routes>
+        
+      </div>
+    </Router>
+ 
   );
 }
 
